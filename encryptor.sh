1e8b238753ec93880c89c5b5f21858b272f4254b
KhDmytrii <khomenkodima8800@gmail.com>
2025-03-26 19:18:35 +0200

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -6,17 +6,30 @@ if [ "$#" -ne 1 ]; then
 fi
 
 target_folder="$1"
+
+if [ ! -d "$target_folder" ]; then
+    echo "Directory '$target_folder' does not exist."
+    exit 1
+fi
+
 encrypted_folder="${target_folder}_encrypted"
-random_key_file="random_key.txt"
+keys_folder="${target_folder}_keys"
+random_bytes_file="random_bytes.txt"
 
 mkdir -p "$encrypted_folder"
+mkdir -p "$keys_folder"
 
-> "$random_key_file"
+> "$random_bytes_file"
 
 find "$target_folder" -type f | while read -r clean_file; do
     random_key=$(openssl rand -hex 16)
-    echo "$(basename "$clean_file"): $random_key" >> "$random_key_file"
-    encrypted_file="$encrypted_folder/$(basename "$clean_file")"
-    cp "$clean_file" "$encrypted_file"
-    ./mixer < "$clean_file" > "$encrypted_file" 2>> random_bytes.txt
+    relative_path="${clean_file#$target_folder/}"
+    encrypted_file="$encrypted_folder/${relative_path%.txt}_encrypted.txt"
+    keys_file="$keys_folder/${relative_path%.txt}_keys.txt"
+
+    mkdir -p "$(dirname "$encrypted_file")"
+    mkdir -p "$(dirname "$keys_file")"
+
+    echo "$random_key" > "$keys_file"
+    ./mixer < "$clean_file" > "$encrypted_file" 2>> "$random_bytes_file"
 done
