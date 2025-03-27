705bf3b58300de6e741da9e35ec4324400d5b888
KhDmytrii <khomenkodima8800@gmail.com>
2025-03-27 18:25:40 +0200

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -14,15 +14,11 @@ fi
 
 encrypted_folder="${target_folder}_encrypted"
 keys_folder="${target_folder}_keys"
-random_bytes_file="random_bytes.txt"
 
 mkdir -p "$encrypted_folder"
 mkdir -p "$keys_folder"
 
-> "$random_bytes_file"
-
 find "$target_folder" -type f | while read -r clean_file; do
-    random_key=$(openssl rand -hex 16)
     relative_path="${clean_file#$target_folder/}"
     encrypted_file="$encrypted_folder/${relative_path%.txt}_encrypted.txt"
     keys_file="$keys_folder/${relative_path%.txt}_keys.txt"
@@ -30,6 +26,5 @@ find "$target_folder" -type f | while read -r clean_file; do
     mkdir -p "$(dirname "$encrypted_file")"
     mkdir -p "$(dirname "$keys_file")"
 
-    echo "$random_key" > "$keys_file"
-    ./mixer < "$clean_file" > "$encrypted_file" 2>> "$random_bytes_file"
+    ./mixer "$keys_file" < "$clean_file" > "$encrypted_file"
 done
